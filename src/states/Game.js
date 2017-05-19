fd2514f65fb9ba509aeb7aca01d067732a29a2d6
FabienDeshayes <fabien.deshayes@gmail.com>
2017-05-19 22:32:37 +0100

    Add isometric tiles PoC

--- src/states/Game.js
+++ src/states/Game.js
@@ -7,8 +7,8 @@ export default class extends Phaser.State {
   preload () {}
 
   create () {
-    const bannerText = 'Phaser + ES6 + Webpack'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
+    const bannerText = 'Fantastic Creatures'
+    let banner = this.add.text(this.world.centerX, 30, bannerText)
     banner.font = 'Bangers'
     banner.padding.set(10, 16)
     banner.fontSize = 40
@@ -23,12 +23,40 @@ export default class extends Phaser.State {
       asset: 'mushroom'
     })
 
-    this.game.add.existing(this.mushroom)
+    this.isoGroup = this.add.group()
+
+    // Let's make a load of cubes on a grid, but do it back-to-front so they get added out of order.
+    let cube
+    for (var xx = 256; xx > 0; xx -= 48) {
+       for (var yy = 256; yy > 0; yy -= 48) {
+           // Create a cube using the new game.add.isoSprite factory method at the specified position.
+           // The last parameter is the group you want to add it to (just like game.add.sprite)
+           cube = game.add.isoSprite(xx, yy, 0, 'mushroom', 0, this.isoGroup)
+           cube.anchor.set(0.5)
+
+           // Store the old messed up ordering so we can compare the two later.
+           cube.oldZ = cube.z
+       }
+    }
+
+    // Just a var so we can tell if the group is this.sorted or not.
+    this.sorted = false
+
+    // Toggle sorting on click/tap.
+    game.input.onDown.add(function () {
+       this.sorted = !this.sorted
+       if (this.sorted) {
+           this.game.iso.simpleSort(this.isoGroup)
+       }
+       else {
+           this.isoGroup.sort('oldZ')
+       }
+    }, this)
   }
 
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+      game.debug.text('fps: ' + this.game.time.fps || '--', 2, 14, '#a7aebe')
     }
   }
 }
